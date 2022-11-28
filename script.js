2f06fd3ac771e99c055bc4ebac955489170a9b58
confett0 <118047961+confett0@users.noreply.github.com>
2022-11-28 16:50:46 +0100

    Add a function that plays a round of rock paper scissors

--- script.js
+++ script.js
@@ -10,3 +10,45 @@ function getComputerChoice() {
 }
 
 const computerSelection = getComputerChoice();
+
+// Play a round 
+
+function playRound(playerSelection, computerSelection) {
+
+    if (computerSelection === 'Rock') {
+
+        if (playerSelection == 'Rock') {
+            return "It's tie! Rock ties with rock.";
+        } else if (playerSelection == 'Paper') {
+            return "You win! Paper beats rock.";
+        } else if (playerSelection == 'Scissors') {
+            return "You lose! Rock beats scissors.";
+        }
+
+    } else if (computerSelection === 'Paper') {
+
+        if (playerSelection == 'Rock') {
+            return "You lose! Paper beats rock.";
+        } else if (playerSelection == 'Paper') {
+            return "It's tie! Paper ties with paper.";
+        } else if (playerSelection == 'Scissors') {
+            return "You win! Scissors beats paper.";
+        }
+
+    } else if (computerSelection === 'Scissors') {
+
+        if (playerSelection == 'Rock') {
+            return "You win! Rock beats scissors.";
+        } else if (playerSelection == 'Paper') {
+            return "You lose! Scissors beats paper.";
+        } else if (playerSelection == 'Scissors') {
+            return "It's a tie! Scissors ties with scissors.";
+        }
+
+    }
+
+}
+
+const playerSelection = "Rock";
+//const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
