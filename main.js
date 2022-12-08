b04b0304dcb29cbc154a0d173b8a687fd41c644c
confett0 <118047961+confett0@users.noreply.github.com>
2022-12-08 15:47:32 +0100

    Rewrite win round logic

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 // Rock Paper Scissors with GUI
 
-// Global variables
+// Global variables and HTML elements
 
 let computerScore = 0;
 let playerScore = 0;
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const result = document.getElementById('results');
 
 // Get computer choice
 
@@ -12,43 +16,55 @@ function getComputerChoice() {
     return choices[Math.floor((Math.random() * choices.length))];
 }
 
+// Play round 
+
+function playRound(e) {
+    const computerSelection = getComputerChoice();
+    const playerSelection = e.target.id;
+    winRound(computerSelection, playerSelection);
+    console.log(playerScore);
+    console.log(computerScore);
+}
+
 // Win round
 
 function winRound(computerChoice, playerChoice) {
 
-    if (computerChoice === playerChoice) {
-        console.log("It's a tie");
-    } else if (
-        playerChoice === "rock" && computerChoice === "scissors" ||
-        playerChoice === "paper" && computerChoice === "rock" ||
-        playerChoice === "scissors" && computerChoice === "paper") {
-        playerScore++;
-        console.log("You win this round!");
+    if (computerChoice === 'rock') {
+        if (playerChoice === 'rock') {
+            console.log(`Computer chose ${computerChoice}: it's a tie!`);
+        } else if (playerChoice === 'paper') {
+            playerScore++;
+            console.log(`Computer chose ${computerChoice}: you win!`);
+        } else {
+            computerScore++;
+            console.log(`Computer chose ${computerChoice}: you lose!`);
+        }
+    } else if (computerChoice === 'paper') {
+        if (playerChoice === 'rock') {
+            computerScore++;
+            console.log(`Computer chose ${computerChoice}: you lose!`);
+        } else if (playerChoice === 'paper') {
+            console.log(`Computer chose ${computerChoice}: it's a tie!`);
+        } else {
+            playerScore++;
+            console.log(`Computer chose ${computerChoice}: you win!`);
+        }
     } else {
-        computerScore++;
-        console.log("You lose this round!");
+        if (playerChoice === 'rock') {
+            playerScore++;
+            console.log(`Computer chose ${computerChoice}: you win!`);
+        } else if (playerChoice === 'paper') {
+            computerScore++;
+            console.log(`Computer chose ${computerChoice}: you lose!`);
+        } else {
+            console.log(`Computer chose ${computerChoice}: it's a tie!`);
+        }
     }
-
-}
-
-// Play round 
-
-function playRound(e) {
-    let computerSelection = getComputerChoice();
-    winRound(computerSelection, e.target.id);
-
 }
 
 // Get player choice 
 
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissors = document.getElementById('scissors');
-
 rock.addEventListener('click',playRound);
 paper.addEventListener('click',playRound);
 scissors.addEventListener('click',playRound);
-
-
-
-
